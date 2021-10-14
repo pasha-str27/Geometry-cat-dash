9ea292dd1a0d57bf298d3bec4e41ce45e00611a0
pashan27 <pashan27@users.noreply.github.com>
2021-10-14 21:23:57 +0300

    added some UI to main menu

---
+++ Assets/Scripts/LoadStatsValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadStatsValue : MonoBehaviour
+{
+    public string valueName;
+
+    void Start()
+    {
+        var text = GetComponent<Text>();
+
+        if (PlayerPrefs.HasKey(valueName))
+            text.text = PlayerPrefs.GetInt(valueName).ToString();
+
+        //text.resizeTextForBestFit = false;
+        //text.fontSize = gameObject.transform.parent.transform.GetChild(0).GetComponent<Text>().fontSize;
+    }
+}
