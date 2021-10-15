225477cdc9971931f9f930d7139e1ed4431d1384
pashan27 <pashan27@users.noreply.github.com>
2021-10-15 12:00:38 +0300

    added shop logic

---
+++ Assets/Scripts/ShopButtonInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButtonInfo : MonoBehaviour
+{
+    public bool isBuyedSkin = false;
+    public bool isChosenSkin = false;
+    public string skinName;
+    public int price = 3;
+
+    private void Start()
+    {
+        transform.GetChild(0).GetComponent<Text>().text = price.ToString() + " звезди";
+    }
+}
---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour
+{
+    public int starsCount = 200;
+    public Text starsValueLabel;
+    public Button[] shopButtons;
+    Button choosenSkin;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Stars"))
+            starsCount = PlayerPrefs.GetInt("Stars");
+
+        UpdateShop();
+    }
+
+    void UpdateShop()
+    {
+        var info = shopButtons[0].GetComponent<ShopButtonInfo>();
+
+        if(!PlayerPrefs.HasKey("chosenSkin"))
+        {
+            PlayerPrefs.SetString("chosenSkin", info.skinName);
+            PlayerPrefs.SetInt(info.skinName + "buyed", 1);
+            info.isBuyedSkin = true;
+
+            PlayerPrefs.SetInt(info.skinName + "choosen", 1);
+            info.isChosenSkin = true;
+
+            choosenSkin = shopButtons[0];
+        }
+
+        for (int i = 0; i < shopButtons.Length; ++i) 
+        {
+            info = shopButtons[i].GetComponent<ShopButtonInfo>();
+
+            if (PlayerPrefs.HasKey(info.skinName + "buyed"))
+                info.isBuyedSkin = true;
+
+            if (PlayerPrefs.HasKey(info.skinName + "choosen") && PlayerPrefs.GetString("chosenSkin") == info.skinName) 
+            {
+                if(choosenSkin)
+                {
+                    choosenSkin.GetComponent<ShopButtonInfo>().isChosenSkin = false;
+                    choosenSkin.gameObject.SetActive(true);
+                    choosenSkin.transform.GetChild(0).GetComponent<Text>().text = "вибрать";
+                }
+                choosenSkin = shopButtons[i];
+                info.isChosenSkin = true;
+            }
+
+            if (info.isBuyedSkin)
+                shopButtons[i].transform.GetChild(0).GetComponent<Text>().text = "вибрать";
+
+            if (info.isChosenSkin)
+                shopButtons[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void TryBuySkin(Button button)
+    {
+        var buttonInfo = button.GetComponent<ShopButtonInfo>();
+
+        if(!button.GetComponent<ShopButtonInfo>().isBuyedSkin)
+        {
+            if (starsCount >= buttonInfo.price)
+            {
+                starsCount -= buttonInfo.price;
+                starsValueLabel.text = starsCount.ToString();
+                buttonInfo.isBuyedSkin = true;
+                button.transform.GetChild(0).transform.GetComponent<Text>().text = "вибрать";
+                PlayerPrefs.SetInt(buttonInfo.skinName + "buyed", 1);
+            }
+
+            return;
+        }
+
+
+        button.transform.GetChild(0).transform.GetComponent<Text>().text = "вибрать";
+        PlayerPrefs.SetInt(buttonInfo.skinName + "choosen", 1);
+        PlayerPrefs.SetString("chosenSkin", buttonInfo.skinName);
+        buttonInfo.isChosenSkin = true;
+
+        choosenSkin.transform.GetChild(0).transform.GetComponent<Text>().text = "вибрать";
+        choosenSkin.gameObject.SetActive(true);
+        choosenSkin = button;
+        choosenSkin.gameObject.SetActive(false);
+    }
+}
